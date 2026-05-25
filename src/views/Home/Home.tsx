d4ae21170ae1ee05a0977704ea0f01be69c28243
oliver <oliver.quail3@gmail.com>
2026-05-25 11:00:48 +1000

    Added some context to home page

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,10 +1,11 @@
+import header from "../../assets/header.png";
 
 
 const Home = () => {
     return (
-        <section>
-            <p>Home</p>
-        </section>
+        <header className="w-full h-screen flex flex-col items-center" style={{ backgroundImage: `url(${header})` }}>
+            <h1 className="text-white scroll-m-20 text-4xl font-extrabold tracking-tight text-balance">Aborignal Art Gallery</h1>
+        </header>
     )
 }
 
