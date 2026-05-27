64526416a8674d9554ef2e545c4a9e91d8e0cad3
oliver <oliver.quail3@gmail.com>
2026-05-27 12:33:47 +1000

    Added artist page code

--- src/views/Artist/Artist.tsx
+++ src/views/Artist/Artist.tsx
@@ -1,9 +1,44 @@
+import type { artistDetails } from "@/types/artistDetails";
+import { useEffect, useState } from "react";
 
 
 const Artist = () => {
+
+    const currentUrl = window.location.href;
+
+    const [artist, setArtist] = useState<artistDetails | null>();
+    
+    useEffect(() => {
+
+        var id = currentUrl.split("/")[currentUrl.split("/").length -1];
+        console.log(id);
+        if(artist == null) {
+            fetch("/api/artist/" + id, {method: "GET"}).then((response :Response) => {
+                response.json().then((jsonData) => {
+                    console.log(jsonData);
+                    setArtist(jsonData as artistDetails);
+                });
+            });
+        }
+    });
+
     return (
         <section>
-            <p>Artist</p>
+            <h1>Artist</h1>
+            <section className="flex w-full">
+                <section className="w-[30%]">
+                    <img src={"../../" + artist?.picture} className="w-full"/>
+                </section>
+                <aside>
+                    <h1>{artist?.name}</h1>
+
+                </aside>
+            </section>
+            <section className="w-full">
+                <h2>My work</h2>
+
+            </section>
+
         </section>
     )
 }
