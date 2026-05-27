a98ec2f9f19ed32da30f21eb5f956a99a0ab6c21
oliver <oliver.quail3@gmail.com>
2026-05-27 10:02:23 +1000

    Added authorization context

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,29 @@ import Artist from "./views/Artist/Artist"
 import Gallery from "./views/Gallery/Gallery"
 import Login from "./views/Login/Login"
 import NavBar from "./components/user/NavBar"
+import { createContext, useContext } from "react"
 
 export function App() {
+
+  const AuthorizationContext = createContext({authorization: "", setAuthorization: () => {}});
+
+  const {authorization, setAuthorization} = useContext(AuthorizationContext);
+
   return (
     <main className="w-full">
-      <BrowserRouter>
-      <NavBar />
-        <Routes>
-          <Route path={VIEWS.INDEX} element={<Home />} />
-          <Route path={VIEWS.ART} element={<Art />} />
-          <Route path={VIEWS.ARTIST} element={<Artist />} />
-          <Route path={VIEWS.GALLERY} element={<Gallery />} />
-          <Route path={VIEWS.LOGIN} element={<Login />} />
+      <AuthorizationContext.Provider value={{authorization, setAuthorization}}>
+        <BrowserRouter>
+        <NavBar />
+          <Routes>
+            <Route path={VIEWS.INDEX} element={<Home />} />
+            <Route path={VIEWS.ART} element={<Art />} />
+            <Route path={VIEWS.ARTIST} element={<Artist />} />
+            <Route path={VIEWS.GALLERY} element={<Gallery />} />
+            <Route path={VIEWS.LOGIN} element={<Login />} />
 
-        </Routes>
-      </BrowserRouter>
+          </Routes>
+        </BrowserRouter>
+      </AuthorizationContext.Provider>
     </main>
   )
 }
