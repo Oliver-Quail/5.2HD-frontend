cf0ff8d2187da8a44503410ccaf3dcdb53d22842
oliver <oliver.quail3@gmail.com>
2026-05-29 09:47:06 +1000

    Added some placeholder text to home page

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -3,9 +3,19 @@ import header from "../../assets/header.png";
 
 const Home = () => {
     return (
-        <header className="w-full h-screen flex flex-col items-center" style={{ backgroundImage: `url(${header})` }}>
-            <h1 className="text-white scroll-m-20 text-4xl font-extrabold tracking-tight text-balance">Aborignal Art Gallery</h1>
-        </header>
+        <article>
+            <header className="w-full h-screen flex flex-col items-center" style={{ backgroundImage: `url(${header})` }}>
+                <h1 className="text-white scroll-m-20 text-4xl font-extrabold tracking-tight text-balance">Aborignal Art Gallery</h1>
+            </header>
+            <section>
+                <h2>About us</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </section>
+            
+        </article>
+
     )
 }
 
